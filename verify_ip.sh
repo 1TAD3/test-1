39b604be5bbb2f211d53876fdff009c0a27b7bf0
1TAD3 <tiberiu13anton@gmail.com>
2024-11-17 16:41:52 +0000

    Updated script to make use of a shell function

--- verify_ip.sh
+++ verify_ip.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-cat /etc/hosts | while read -r ip name _; do
+
+validate_host_entry() {
+    local ip="$1"
+    local name="$2"
+
     if [[ "$ip" == \#* || -z "$ip" || -z "$name" ]]; then
-        continue
+        return
     fi
+
+    local resolved_ip
     resolved_ip=$(nslookup "$name" 2>/dev/null | awk '/^Address: / {print $2}' | tail -n1)
+
     if [[ -n "$resolved_ip" && "$resolved_ip" != "$ip" ]]; then
         echo "Bogus IP for $name in /etc/hosts!"
     fi
-done
+}
+
+while read -r ip name _; do
+    validate_host_entry "$ip" "$name"
+done < /etc/hosts
+
